b16eb0b860af9b90e3a6726de7937c76ea747b92
Gojinevschi04 <annagojinevschi@gmail.com>
2023-11-19 13:48:00 +0200

    add  methods

--- lagrange_syms.py
+++ lagrange_syms.py
@@ -1,30 +1,35 @@
 import sympy as sp
-from typing import List
+from decimal import Decimal
 
 
 def print_polynomial(polynomial_expression: sp.Expr, polynomial_degree: int) -> None:
     print(f"Polynomial of degree {polynomial_degree}:")
-    print(sp.pretty(polynomial_expression))
+    print(polynomial_expression)
 
 
 def interpolate_lagrange(
-        x_data_points: List[float], y_function_values: List[float]
+        x_data_points: list[Decimal], y_function_values: list[Decimal]
 ) -> sp.Expr:
     if len(x_data_points) != len(y_function_values) or len(x_data_points) == 0:
         raise ValueError(
             "Input lists x_data_points and y_function_values must have the same non-zero length."
         )
 
+    number_of_values: int = len(x_data_points)
     independent_variable: sp.Symbol = sp.symbols("x")
-
     interpolated_polynomial: sp.Expr = sp.sympify(0)
+    lagrange_term: sp.Expr
 
-    for current_point_index in range(len(x_data_points)):
+    for current_point_index in range(number_of_values):
         function_value: sp.Expr = sp.sympify(y_function_values[current_point_index])
-        function_value *= compute_lagrange_term(
+
+        lagrange_term = compute_lagrange_term(
             independent_variable, x_data_points, current_point_index
         )
-        interpolated_polynomial += function_value
+
+        function_value = sp.Mul(function_value, lagrange_term)
+
+        interpolated_polynomial = sp.Add(interpolated_polynomial, function_value)
 
     simplified_polynomial: sp.Expr = sp.simplify(interpolated_polynomial).evalf()
 
@@ -33,155 +38,78 @@ def interpolate_lagrange(
 
 def compute_lagrange_term(
         independent_variable: sp.Symbol,
-        x_data_points: List[float],
+        x_data_points: list[Decimal],
         current_point_index: int,
 ) -> sp.Expr:
     term: sp.Expr = sp.Integer(1)
-    for j in range(len(x_data_points)):
-        if j == current_point_index:
+    number_of_values: int = len(x_data_points)
+
+    for index in range(number_of_values):
+
+        if index == current_point_index:
             continue
-        term *= (independent_variable - x_data_points[j]) / (
-                x_data_points[current_point_index] - x_data_points[j]
-        )
+
+        numerator: sp.Expr = independent_variable - x_data_points[index]
+        denominator: sp.Expr = sp.sympify(x_data_points[current_point_index]) - sp.sympify(x_data_points[index])
+
+        term = sp.Mul(term, sp.Pow(denominator, -1) * numerator)
+
     return term
 
 
-def compute_divided_difference(x0: float, x1: float, y0: float, y1: float) -> float:
+def compute_divided_difference(x0: Decimal, x1: Decimal, y0: Decimal, y1: Decimal) -> Decimal:
     return (y0 * x1 - y1 * x0) / (x1 - x0)
 
 
-def build_interpolation_table(
-        interpolation_point: float,
-        x_data_points: List[float],
-        y_function_values: List[float],
-) -> List[List[float]]:
-    table_headers: List[str] = ["X Value", "Function Value", "Xi - a"]
-    interpolation_table: List[List[float]] = []
+def get_lagrange_polynomials_values(x_data_points: list[Decimal], y_function_values: list[Decimal],
+                                    alpha: Decimal) -> Decimal:
+    initial_polynom_degree: int = len(x_data_points)
+    value: int = len(x_data_points) - 1
+    current_approximation: Decimal = Decimal(0)
+    previous_approximation: Decimal = Decimal(0)
 
-    for current_point_index in range(len(x_data_points)):
-        x_value: float = x_data_points[current_point_index]
-        function_value: float = y_function_values[current_point_index]
-        x_minus_a: float = round(x_value - interpolation_point, 5)
-        table_row: List[float] = [x_value, function_value, x_minus_a]
-        interpolation_table.append(table_row)
+    while value > 0:
+        lagrange_polynomial = interpolate_lagrange(x_data_points[:-value], y_function_values[:-value])
+        current_approximation = lagrange_polynomial.subs('x', alpha)
 
-    return interpolation_table
+        # if abs(current_approximation - previous_approximation) < epsilon:
+        #     break
 
+        print(f"Eroarea: {abs(current_approximation - previous_approximation)}")
+        previous_approximation = current_approximation
 
-def update_interpolation_table(
-        interpolation_table: List[List[float]],
-) -> List[List[float]]:
-    for current_point_index in range(len(interpolation_table)):
-        for current_term_index in range(
-                1, len(interpolation_table[current_point_index])
-        ):
-            try:
-                x0, x1, y0, y1 = (
-                    interpolation_table[current_point_index - current_term_index + 2][
-                        2
-                    ],
-                    interpolation_table[current_point_index][2],
-                    interpolation_table[current_point_index - 1][
-                        current_term_index - 2
-                        ],
-                    interpolation_table[current_point_index][current_term_index - 2],
-                )
-            except:
-                pass
-            if current_term_index > 3:
-                y0 = interpolation_table[current_point_index - 1][
-                    current_term_index - 1
-                    ]
-                y1 = interpolation_table[current_point_index][current_term_index - 1]
-            if interpolation_table[current_point_index][current_term_index] == 0:
-                result = compute_divided_difference(x0, x1, y0, y1)
-                interpolation_table[current_point_index][current_term_index] = round(
-                    result, 4
-                )
+        print(f"m = {initial_polynom_degree - value}, Ln{initial_polynom_degree - value} = {current_approximation}")
+        value -= 1
 
-            error = abs(
-                interpolation_table[current_point_index][current_term_index]
-                - interpolation_table[current_point_index - 1][current_term_index - 1]
-            )
-
-            if error < epsilon:
-                break
-
-    return interpolation_table
+    return current_approximation
 
 
-def print_interpolation_table(interpolation_table: List[List[float]]) -> None:
-    table_headers = ["X Value", "Function Value", "X - a"]
-    formatted_table_headers = [
-        header.replace("Lagrange Term_", ",") for header in table_headers
-    ]
-    formatted_table = [
-        ["{:10}".format(item) for item in row]
-        for row in [formatted_table_headers] + interpolation_table
-    ]
-    formatted_table_strings = ["|".join(row) + "|" for row in formatted_table]
-    print("\n".join(formatted_table_strings))
+epsilon: Decimal = Decimal(10 ** -4)
+interpolation_point: Decimal = Decimal(-0.532)
 
+x_data_points: list[Decimal] = [Decimal('-1.432'), Decimal('-0.675'), Decimal('1.439'), Decimal('2.567'),
+                                Decimal('3.486'),
+                                Decimal('4.910'),
+                                Decimal('5.763')]
 
-def aitken_interpolation(
-        interpolation_point: float,
-        x_data_points: List[float],
-        y_function_values: List[float],
-) -> float:
-    if len(x_data_points) != len(y_function_values) or len(x_data_points) == 0:
-        raise ValueError(
-            "Input lists x_data_points and y_function_values must have the same non-zero length."
-        )
+y_function_values: list[Decimal] = [Decimal('7.67103'), Decimal('5.45321'), Decimal('3.76129'), Decimal('0.56741'),
+                                    Decimal('-1.5630'),
+                                    Decimal('0.7684'),
+                                    Decimal('2.56793')]
 
-    interpolation_table: List[List[float]] = build_interpolation_table(
-        interpolation_point, x_data_points, y_function_values
-    )
-    interpolation_table = update_interpolation_table(interpolation_table)
-
-    result: float = interpolation_table[-1][-1]
-
-    print_interpolation_table(interpolation_table)
-
-    print(f"f({interpolation_point}) ≈ L({interpolation_point}) = {result}")
-
-    return result
-
-
-epsilon: float = 10 ** -4
-
-interpolation_point: float = 0.204
-x_data_points: List[float] = [-1.432, -0.675, 1.439, 2.567, 3.486, 4.910, 5.763]
-
-y_function_values: List[float] = [
-    7.67103,
-    5.45321,
-    3.76129,
-    0.56741,
-    -1.5630,
-    0.7684,
-    2.56793,
-]
-
-polynomial_degree: int = len(x_data_points)
+polynomial_degree: int = len(x_data_points) - 1
 
 interpolated_polynomial: sp.Expr = interpolate_lagrange(
     x_data_points, y_function_values
 )
+
 print_polynomial(interpolated_polynomial, polynomial_degree)
 
-print(
-    f"L{polynomial_degree}({interpolation_point}) = {interpolated_polynomial.subs('x', interpolation_point)}"
-)
-print(
-    "For a =",
-    interpolation_point,
-    "Ln(",
-    interpolation_point,
-    ") =",
-    interpolated_polynomial.subs("x", interpolation_point),
-    polynomial_degree,
-)
+# interpolated_polynomial_value: Decimal = interpolated_polynomial.subs('x', interpolation_point)
 
-aitken_interpolation_result: float = aitken_interpolation(
-    interpolation_point, x_data_points, y_function_values
-)
+# print(
+#     f"L{polynomial_degree}({round(interpolation_point, 3)}) = {round(interpolated_polynomial_value, 15)}"
+# )
+
+# print("\n Pentru cazul in care m < n:")
+# get_lagrange_polynomials_values(x_data_points, y_function_values, interpolation_point)
--- main.py
+++ main.py
@@ -1,112 +1,300 @@
-from decimal import Decimal
-
-decimal_list = list[Decimal]
-
-
-def has_unique_values(input_list: decimal_list) -> bool:
-    return len(set(input_list)) == len(input_list)
-
-
-def print_values(x_values: decimal_list, y_values: decimal_list) -> None:
-    print("\nInput values are:\n")
-    print("X", end="\t")
-
-    for x in x_values:
-        print(f"{x:.6f}", end="\t")
-    print("\nY", end="\t")
-
-    for y in y_values:
-        print(f"{y:.6f}", end="\t")
-    print()
-
-
-def print_lagrange_interpolation_polynomial(intermediate_values: decimal_list):
-    degree = len(intermediate_values) - 1
-    print("\n1) Lagrange Interpolation Polynomial Ln is:")
-    print(f"Ln(x) = {intermediate_values[degree]:.6f}X^{degree}", end=" ")
-
-    current_degree = degree - 1
-
-    for k in range(degree - 1, 0, -1):
-        print(f"+{intermediate_values[k]:.6f}X^{current_degree}", end=" ")
-
-        if k == degree - 4:
-            print("\n\t", end="")
-        current_degree -= 1
-
-    print(f"+{intermediate_values[0]:.6f}")
-
-
-def calculate_coefficients(x_data: decimal_list) -> decimal_list:
-    degree = len(x_data)
-    coefficients = [Decimal('0.0') for _ in range(degree)]
-    coefficients[0] = Decimal('1')
-
-    for i in range(degree - 1):
-        coefficients[i + 1] = coefficients[i]
-
-        for j in range(i, 0, -1):
-            coefficients[j] = coefficients[j - 1] - coefficients[j] * x_data[i]
-        coefficients[0] = -coefficients[0] * x_data[i]
-
-    return coefficients
-
-
-def calculate_intermediate_values(x_data: decimal_list, y_data: decimal_list,
-                                  coefficients: decimal_list) -> decimal_list:
-    degree = len(x_data)
-    intermediate_values = [Decimal('0.0') for _ in range(degree + 1)]
-    temp_values = [Decimal('0.0') for _ in range(degree + 1)]
-
-    for i in range(degree):
-        product = Decimal('1')
-        for j in range(degree):
-            if i != j:
-                product *= (x_data[i] - x_data[j])
-
-        for k in range(degree, -1, -1):
-            if k == 0:
-                temp_values[k] = Decimal('0.0')
-            else:
-                temp_values[k] = coefficients[k - 1] + x_data[i] * temp_values[k - 1]
-
-        for k in range(degree + 1):
-            intermediate_values[k] += y_data[i] * temp_values[k] / product
-
-    return intermediate_values
-
-
-def lagrange_interpolation(x_data: decimal_list, y_data: decimal_list, target_x: Decimal) -> Decimal:
-    coefficients = calculate_coefficients(x_data)
-    intermediate_values = calculate_intermediate_values(x_data, y_data, coefficients)
-
-    degree = len(x_data) - 1
-    interpolated_value = intermediate_values[degree]
-
-    for i in range(1, degree + 1):
-        interpolated_value = target_x * interpolated_value + intermediate_values[degree - i]
-
-    return interpolated_value
-
-
-x_values = [Decimal('0.104'), Decimal('0.205'), Decimal('0.310'), Decimal('0.401'), Decimal('0.507'), Decimal('0.618'), Decimal('0.721')]
-
-y_values = [Decimal('4.96713'), Decimal('6.811347'), Decimal('8.76712'), Decimal('10.16147'), Decimal('9.12347'), Decimal('7.26493'), Decimal('5.37149')]
-
-target_x_value: Decimal = Decimal(0.205)
-
-data_length = len(x_values)
-
-result = lagrange_interpolation(x_values, y_values, target_x_value)
-
-print_lagrange_interpolation_polynomial(
-    calculate_intermediate_values(x_values, y_values, calculate_coefficients(x_values)))
-
-print(
-    f"\n2) The value of the function f(x) at x = {target_x_value:.3f} using the Lagrange interpolation polynomial is: f(x) = {result:.6f}")
-
-print("\nCase where m < n:")
-new_length = data_length - 3
-reduced_x_values, reduced_y_values = x_values[:new_length], y_values[:new_length]
-print_values(reduced_x_values, reduced_y_values)
-lagrange_interpolation(reduced_x_values, reduced_y_values, target_x_value)
+# def update_interpolation_table(
+#         interpolation_table: List[List[float]],
+# ) -> List[List[float]]:
+#     number_of_values: int = len(interpolation_table)
+#
+#     for current_point_index in range(1, number_of_values):
+#         print(f"i = {current_point_index}")
+#
+#         for current_term_index in range(
+#                 1, len(interpolation_table[current_point_index])
+#         ):
+#             print(f"j = {current_point_index}")
+#
+#             try:
+#                 x0, x1, y0, y1 = (
+#                     interpolation_table[current_point_index - current_term_index + 2][
+#                         2
+#                     ],
+#                     interpolation_table[current_point_index][2],
+#                     interpolation_table[current_point_index - 1][
+#                         current_term_index - 2
+#                         ],
+#                     interpolation_table[current_point_index][current_term_index - 2],
+#                 )
+#
+#                 print(f"x0 = {x0}, y0 = {y0}")
+#                 print(f"x1 = {x1}, y1 = {y1}")
+#
+#             except:
+#                 pass
+#
+#             if current_term_index > 3:
+#                 y0 = interpolation_table[current_point_index - 1][
+#                     current_term_index - 1
+#                     ]
+#                 y1 = interpolation_table[current_point_index][current_term_index - 1]
+#
+#             if interpolation_table[current_point_index][current_term_index] == 0:
+#                 result = compute_divided_difference(x0, x1, y0, y1)
+#                 interpolation_table[current_point_index][current_term_index] = round(
+#                     result, 10
+#                 )
+#
+#                 print(f"Result: {result}\n")
+#
+#             error = abs(
+#                 interpolation_table[current_point_index][current_term_index]
+#                 - interpolation_table[current_point_index - 1][current_term_index-1]
+#             )
+#
+#             print(f"Eroarea: {error}\n")
+#
+#             # if error < epsilon:
+#             #     break
+#
+#     return interpolation_table
+#
+# import sympy as sp
+# from typing import List
+# from pprint import pprint
+#
+#
+# def print_polynomial(polynomial_expression: sp.Expr, polynomial_degree: int) -> None:
+#     print(f"Polynomial of degree {polynomial_degree}:")
+#     print(polynomial_expression)
+#
+#
+# def interpolate_lagrange(
+#         x_data_points: List[float], y_function_values: List[float]
+# ) -> sp.Expr:
+#     if len(x_data_points) != len(y_function_values) or len(x_data_points) == 0:
+#         raise ValueError(
+#             "Input lists x_data_points and y_function_values must have the same non-zero length."
+#         )
+#
+#     number_of_values: int = len(x_data_points)
+#     independent_variable: sp.Symbol = sp.symbols("x")
+#     interpolated_polynomial: sp.Expr = sp.sympify(0)
+#
+#     for current_point_index in range(number_of_values):
+#         function_value: sp.Expr = sp.sympify(y_function_values[current_point_index])
+#
+#         function_value *= compute_lagrange_term(
+#             independent_variable, x_data_points, current_point_index
+#         )
+#
+#         interpolated_polynomial += function_value
+#
+#     simplified_polynomial: sp.Expr = sp.simplify(interpolated_polynomial).evalf()
+#
+#     return simplified_polynomial
+#
+#
+# def compute_lagrange_term(
+#         independent_variable: sp.Symbol,
+#         x_data_points: List[float],
+#         current_point_index: int,
+# ) -> sp.Expr:
+#     term: sp.Expr = sp.Integer(1)
+#     number_of_values: int = len(x_data_points)
+#
+#     for index in range(number_of_values):
+#
+#         if index == current_point_index:
+#             continue
+#
+#         term *= (independent_variable - x_data_points[index]) / (
+#                 x_data_points[current_point_index] - x_data_points[index]
+#         )
+#     return term
+#
+#
+# def compute_divided_difference(x0: float, x1: float, y0: float, y1: float) -> float:
+#     return (y0 * x1 - y1 * x0) / (x1 - x0)
+#
+#
+# def build_interpolation_table(
+#         interpolation_point: float,
+#         x_data_points: List[float],
+#         y_function_values: List[float],
+# ) -> List[List[float]]:
+#     number_of_values: int = len(x_data_points)
+#     table_headers: List[str] = ["X Value", "Function Value", "Xi - a"]
+#     interpolation_table: List[List[float]] = []
+#
+#     for current_point_index in range(number_of_values):
+#         x_value: float = x_data_points[current_point_index]
+#         function_value: float = y_function_values[current_point_index]
+#         x_minus_a: float = round(x_value - interpolation_point, 6)
+#
+#         table_row: List[float] = [x_value, function_value, x_minus_a]
+#
+#         title: str = "L_"
+#
+#         for index in range(current_point_index, 0, -1):
+#
+#             if current_point_index == 0:
+#                 continue
+#
+#             title += f'i-{index}_'
+#             table_row.append(0)
+#
+#         if current_point_index != 0:
+#             table_headers.append(title + 'i')
+#
+#         interpolation_table.append(table_row)
+#
+#     return interpolation_table
+#
+#
+# def update_interpolation_table(
+#         interpolation_table: List[List[float]],
+# ) -> List[List[float]]:
+#     number_of_values: int = len(interpolation_table)
+#
+#     for current_point_index in range(1, number_of_values):
+#         print(f"i = {current_point_index}")
+#
+#         for current_term_index in range(
+#                 1, len(interpolation_table[current_point_index])
+#         ):
+#             print(f"j = {current_point_index}")
+#
+#             try:
+#                 x0, x1, y0, y1 = (
+#                     interpolation_table[current_point_index - current_term_index + 2][
+#                         2
+#                     ],
+#                     interpolation_table[current_point_index][2],
+#                     interpolation_table[current_point_index - 1][
+#                         current_term_index - 2
+#                         ],
+#                     interpolation_table[current_point_index][current_term_index - 2],
+#                 )
+#
+#                 print(f"x0 = {x0}, y0 = {y0}")
+#                 print(f"x1 = {x1}, y1 = {y1}")
+#
+#             except:
+#                 pass
+#
+#             if current_term_index > 3:
+#                 y0 = interpolation_table[current_point_index - 1][
+#                     current_term_index - 1
+#                     ]
+#                 y1 = interpolation_table[current_point_index][current_term_index - 1]
+#
+#             if interpolation_table[current_point_index][current_term_index] == 0:
+#                 result = compute_divided_difference(x0, x1, y0, y1)
+#                 interpolation_table[current_point_index][current_term_index] = round(
+#                     result, 10
+#                 )
+#
+#                 print(f"Result: {result}\n")
+#
+#             error = abs(
+#                 interpolation_table[current_point_index][current_term_index]
+#                 - interpolation_table[current_point_index - 1][current_term_index - 1]
+#             )
+#
+#             print(f"Eroarea: {error}\n")
+#
+#             # if error < epsilon:
+#             #     break
+#
+#     return interpolation_table
+#
+#
+# def print_interpolation_table(interpolation_table: List[List[float]]) -> None:
+#     table_headers = ["X Value", "Function Value", "X - a"]
+#
+#     formatted_table_headers = [
+#         header.replace("Lagrange Term_", ",") for header in table_headers
+#     ]
+#
+#     formatted_table = [
+#         ["{:14}".format(item) for item in row]
+#         for row in [formatted_table_headers] + interpolation_table
+#     ]
+#
+#     formatted_table_strings = ["|".join(row) + "|" for row in formatted_table]
+#
+#     print("\n".join(formatted_table_strings))
+#
+#
+# def aitken_interpolation(
+#         interpolation_point: float,
+#         x_data_points: List[float],
+#         y_function_values: List[float],
+# ) -> float:
+#     if len(x_data_points) != len(y_function_values) or len(x_data_points) == 0:
+#         raise ValueError(
+#             "Input lists x_data_points and y_function_values must have the same non-zero length."
+#         )
+#
+#     interpolation_table: List[List[float]] = build_interpolation_table(
+#         interpolation_point, x_data_points, y_function_values
+#     )
+#     interpolation_table = update_interpolation_table(interpolation_table)
+#
+#     result: float = interpolation_table[-1][-1]
+#
+#     print_interpolation_table(interpolation_table)
+#
+#     # print(f"\nf({interpolation_point}) ≈ L({interpolation_point}) = {result}")
+#
+#     return result
+#
+#
+# def get_lagrange_polynomials_values(x_data_points: List[float], y_function_values: List[float], alpha: float) -> float:
+#     previous_approximation = None
+#     number_of_values: int = len(x_data_points)
+#     current_approximation: float = 0
+#
+#     for index in range(1, number_of_values):
+#         lagrange_polynomial = interpolate_lagrange(x_data_points[:-index], y_function_values[:-index])
+#         current_approximation = lagrange_polynomial.subs('x', alpha)
+#
+#         # if previous_approximation is not None and abs(current_approximation - previous_approximation) < epsilon:
+#         #     break
+#
+#         previous_approximation = current_approximation
+#
+#         print(current_approximation)
+#
+#     return current_approximation
+#
+#
+# epsilon: float = 10 ** -4
+#
+# interpolation_point: float = -0.532
+# x_data_points: List[float] = [-1.432, -0.675, 1.439, 2.567, 3.486, 4.910, 5.763]
+#
+# y_function_values: List[float] = [
+#     7.67103,
+#     5.45321,
+#     3.76129,
+#     0.56741,
+#     -1.5630,
+#     0.7684,
+#     2.56793,
+# ]
+#
+# polynomial_degree: int = len(x_data_points) - 1
+#
+# interpolated_polynomial: sp.Expr = interpolate_lagrange(
+#     x_data_points, y_function_values
+# )
+# # print_polynomial(interpolated_polynomial, polynomial_degree)
+#
+# # print(
+# #     f"L{polynomial_degree}({interpolation_point}) = {interpolated_polynomial.subs('x', interpolation_point)}"
+# # )
+#
+# # aitken_interpolation_result: float = aitken_interpolation(
+# #     interpolation_point, x_data_points, y_function_values
+# # )
+# get_lagrange_polynomials_values(x_data_points, y_function_values, interpolation_point)
+#
+# # schema lui aitken
