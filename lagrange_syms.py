124e427e3d92c7d4fcd355d50bf6af8842f1d4bf
Gojinevschi04 <annagojinevschi@gmail.com>
2023-11-22 12:34:47 +0200

    add  methods

--- lagrange_syms.py
+++ lagrange_syms.py
@@ -3,12 +3,12 @@ from decimal import Decimal
 
 
 def print_polynomial(polynomial_expression: sp.Expr, polynomial_degree: int) -> None:
-    print(f"Polynomial of degree {polynomial_degree}:")
+    print(f"Polinomul de interpolare Lagrange de gradul {polynomial_degree}:")
     print(polynomial_expression)
 
 
 def interpolate_lagrange(
-        x_data_points: list[Decimal], y_function_values: list[Decimal]
+    x_data_points: list[Decimal], y_function_values: list[Decimal]
 ) -> sp.Expr:
     if len(x_data_points) != len(y_function_values) or len(x_data_points) == 0:
         raise ValueError(
@@ -37,44 +37,46 @@ def interpolate_lagrange(
 
 
 def compute_lagrange_term(
-        independent_variable: sp.Symbol,
-        x_data_points: list[Decimal],
-        current_point_index: int,
+    independent_variable: sp.Symbol,
+    x_data_points: list[Decimal],
+    current_point_index: int,
 ) -> sp.Expr:
     term: sp.Expr = sp.Integer(1)
     number_of_values: int = len(x_data_points)
 
     for index in range(number_of_values):
-
         if index == current_point_index:
             continue
 
         numerator: sp.Expr = independent_variable - x_data_points[index]
-        denominator: sp.Expr = sp.sympify(x_data_points[current_point_index]) - sp.sympify(x_data_points[index])
+        denominator: sp.Expr = sp.sympify(
+            x_data_points[current_point_index]
+        ) - sp.sympify(x_data_points[index])
 
         term = sp.Mul(term, sp.Pow(denominator, -1) * numerator)
 
     return term
 
 
-def compute_divided_difference(x0: Decimal, x1: Decimal, y0: Decimal, y1: Decimal) -> Decimal:
-    return (y0 * x1 - y1 * x0) / (x1 - x0)
-
-
-def get_lagrange_polynomials_values(x_data_points: list[Decimal], y_function_values: list[Decimal],
-                                    alpha: Decimal) -> Decimal:
+def get_lagrange_polynomials_values(
+    x_data_points: list[Decimal], y_function_values: list[Decimal], alpha: Decimal
+) -> Decimal:
     initial_polynom_degree: int = len(x_data_points)
     value: int = len(x_data_points) - 1
     current_approximation: Decimal = Decimal(0)
     previous_approximation: Decimal = Decimal(0)
 
     while value > 0:
-        lagrange_polynomial = interpolate_lagrange(x_data_points[:-value], y_function_values[:-value])
-        current_approximation = lagrange_polynomial.subs('x', alpha)
+        lagrange_polynomial = interpolate_lagrange(
+            x_data_points[:-value], y_function_values[:-value]
+        )
+        current_approximation = lagrange_polynomial.subs("x", alpha)
 
         # if abs(current_approximation - previous_approximation) < epsilon:
         #     break
-        print(f"m = {initial_polynom_degree - value}, Ln{initial_polynom_degree - value} = {current_approximation}")
+        print(
+            f"m = {initial_polynom_degree - value}, Ln{initial_polynom_degree - value} = {current_approximation}"
+        )
         value -= 1
 
         print(f"Eroarea: {abs(current_approximation - previous_approximation)}")
@@ -83,18 +85,28 @@ def get_lagrange_polynomials_values(x_data_points: list[Decimal], y_function_val
     return current_approximation
 
 
-epsilon: Decimal = Decimal(10 ** -4)
+epsilon: Decimal = Decimal(10**-4)
 interpolation_point: Decimal = Decimal(-0.532)
 
-x_data_points: list[Decimal] = [Decimal('-1.432'), Decimal('-0.675'), Decimal('1.439'), Decimal('2.567'),
-                                Decimal('3.486'),
-                                Decimal('4.910'),
-                                Decimal('5.763')]
+x_data_points: list[Decimal] = [
+    Decimal("-1.432"),
+    Decimal("-0.675"),
+    Decimal("1.439"),
+    Decimal("2.567"),
+    Decimal("3.486"),
+    Decimal("4.910"),
+    Decimal("5.763"),
+]
 
-y_function_values: list[Decimal] = [Decimal('7.67103'), Decimal('5.45321'), Decimal('3.76129'), Decimal('0.56741'),
-                                    Decimal('-1.5630'),
-                                    Decimal('0.7684'),
-                                    Decimal('2.56793')]
+y_function_values: list[Decimal] = [
+    Decimal("7.67103"),
+    Decimal("5.45321"),
+    Decimal("3.76129"),
+    Decimal("0.56741"),
+    Decimal("-1.5630"),
+    Decimal("0.7684"),
+    Decimal("2.56793"),
+]
 
 polynomial_degree: int = len(x_data_points) - 1
 
@@ -104,7 +116,9 @@ interpolated_polynomial: sp.Expr = interpolate_lagrange(
 
 print_polynomial(interpolated_polynomial, polynomial_degree)
 
-interpolated_polynomial_value: Decimal = interpolated_polynomial.subs('x', interpolation_point)
+interpolated_polynomial_value: Decimal = interpolated_polynomial.subs(
+    "x", interpolation_point
+)
 
 print(
     f"\nL{polynomial_degree}({round(interpolation_point, 3)}) = {round(interpolated_polynomial_value, 15)}"
---
+++ modified.py
@@ -0,0 +1,94 @@
+import copy
+from decimal import Decimal
+from math import exp
+
+
+def print_result(results: list[Decimal]) -> None:
+    # print(f"Solutia: {results[-1]}")
+    count: int = len(results)
+
+    for index in range(count):
+        print(f"Iteratia: {index}")
+        print(f"Rezultatul: {round(results[index], 15)}")
+
+
+def differential_equation(x_value: Decimal, y_value: Decimal) -> Decimal:
+    return Decimal(x_value ** 2 + Decimal(0.2) * y_value ** 2)
+
+
+def euler_method(
+        y_initial: Decimal, start_x: Decimal, end_x: Decimal, step_size: Decimal
+) -> list[Decimal]:
+    solution_values: list[Decimal] = [y_initial]
+    current_x: Decimal = copy.copy(start_x)
+    current_y: Decimal = copy.copy(y_initial)
+
+    while current_x < end_x:
+        current_y += step_size * differential_equation(current_x, current_y)
+        solution_values.append(current_y)
+        current_x += step_size
+
+    return solution_values
+
+
+def modified_euler_method(
+        y_initial: Decimal, start_x: Decimal, end_x: Decimal, step_size: Decimal
+) -> list[Decimal]:
+    solution_values: list[Decimal] = [y_initial]
+    current_x: Decimal = copy.copy(start_x)
+    current_y: Decimal = copy.copy(y_initial)
+
+    while current_x < end_x:
+        k1: Decimal = step_size * differential_equation(current_x, current_y)
+        k2: Decimal = step_size * differential_equation(
+            current_x + (step_size / 2), current_y + (k1 / 2)
+        )
+
+        current_y += k2
+        solution_values.append(current_y)
+        current_x += step_size
+
+    return solution_values
+
+
+def runge_kutta_method(
+        y_initial: Decimal, start_x: Decimal, end_x: Decimal, step_size: Decimal
+) -> list[Decimal]:
+    solution_values: list[Decimal] = [y_initial]
+    current_x: Decimal = copy.copy(start_x)
+    current_y: Decimal = copy.copy(y_initial)
+
+    while current_x < end_x:
+        k1: Decimal = step_size * differential_equation(current_x, current_y)
+        k2: Decimal = step_size * differential_equation(
+            current_x + (step_size / 2), current_y + (k1 / 2)
+        )
+        k3: Decimal = step_size * differential_equation(
+            current_x + (step_size / 2), current_y + (k2 / 2)
+        )
+        k4: Decimal = step_size * differential_equation(
+            current_x + step_size, current_y + k3
+        )
+
+        current_y += (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+        solution_values.append(current_y)
+        current_x += step_size
+
+    return solution_values
+
+
+start_value: Decimal = Decimal(0)
+end_value: Decimal = Decimal(1)
+
+initial_y: Decimal = Decimal(0.2)
+step_value: Decimal = Decimal(0.05)
+
+print("\nMetoda Euler:")
+print_result(euler_method(initial_y, start_value, end_value, step_value))
+
+print("\nMetoda Euler modificat:")
+print_result(modified_euler_method(initial_y, start_value, end_value, step_value))
+
+print("\nMetoda Runge – Kutta:")
+print_result(runge_kutta_method(initial_y, start_value, end_value, step_value))
